393756b99d4b182e74818486297318d2fe736ab5
Michal Babej <michal.babej@tuni.fi>
2026-09-11 16:58:14 +0300

    add some values to the list of hardcoded values for testing fp32 ldexp

--- test_conformance/math_brute_force/common.cpp
+++ test_conformance/math_brute_force/common.cpp
@@ -741,6 +741,8 @@ static const std::vector<float> floatSpecialValues = {
     MAKE_HEX_FLOAT(-0x1.000002p31f, -0x1000002L, 7),
     MAKE_HEX_FLOAT(-0x1.0p31f, -0x1L, 31),
     MAKE_HEX_FLOAT(-0x1.fffffep30f, -0x1fffffeL, 6),
+    0x1.20abd6p-14, // ldexp in POCL
+    -0x1.6a24ep-12, // ldexp in POCL
     -1000.f,
     -100.f,
     -4.0f,
@@ -853,8 +855,8 @@ const std::vector<cl_half> &getHalfSpecialValues() { return halfSpecialValues; }
 static const std::vector<int> intSpecialValues = {
     0,           1,           2,           3,          126,        127,
     128,         0x02000001,  0x04000001,  1465264071, 1488522147, -1,
-    -2,          -3,          -126,        -127,       -128,       -0x02000001,
-    -0x04000001, -1465264071, -1488522147,
+    -2,          -3,          -126,        -127,       -128,       -131,        -133,
+    -0x02000001, -0x04000001, -1465264071, -1488522147,
 };
 
 const std::vector<int> &getIntSpecialValues() { return intSpecialValues; }
